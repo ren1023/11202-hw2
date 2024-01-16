b4f607b84a68d61a4d143316e9e0c8e68b73950d
ren1023 <renyou.1023@gmail.com>
2024-01-16 23:47:53 +0800

    最新消息引入

--- front/news.php
+++ front/news.php
@@ -1,4 +1,5 @@
 
+
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 	<?php 
 	// include "marquee.php";
@@ -9,13 +10,11 @@
 	<h3>更多最新消息顯示區</h3>
 	<hr>
 	<?php $total = $DB->count(['sh' => 1]);
-	$div = 5;
-	$pages = ceil($total / $div);
-	$now = $_GET['p'] ?? 1;
-	$start = ($now - 1) * $div;
-	$news = $News->all("limit $start,$div");
-	
-
+		$div = 5;
+		$pages = ceil($total / $div);
+		$now = $_GET['p'] ?? 1;
+		$start = ($now - 1) * $div;
+		$news = $News->all("limit $start,$div");
 	?>
 	<ol class="ssaa" start='<?= $start + 1; ?>'>
 		<?php
@@ -29,39 +28,40 @@
 			echo "...</li>";
 		}
 		?>
-
 	</ol>
 
 	<div class="cent">
 		<?php
-
+			
 		if ($now > 1) {
+			
 			$prev = $now - 1;
-			echo " <a href='?do=news&p=$prev'><</a> ";
+			echo " <a href='?do=$do&p=$prev'><</a> ";
 		}
 
 
 		for ($i = 1; $i <= $pages; $i++) {
 			$fontsize = ($now == $i) ? '24px' : '16px';
-			echo "<a href='?do=news&p=$i' style='font-size:$fontsize'> $i <a/>";
+			echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i <a/>";
 		}
 
 
 		if ($now < $pages) {
 			$next = $now + 1;
-			echo " <a href='?do=news&p=$next'> > </a> ";
+			echo " <a href='?do=$do&p=$next'> > </a> ";
 		}
 
 		?>
 
 	</div>
 </div>
+</div>
 <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 			<script>
 				$(".sswww").hover(
 					function() {
 						$("#alt").html("<pre>" + $(this).children(".all").html() + "</pre>").css({
-							"top": $(this).offset().top - 50
+							"top": $(this).offset().top - 200
 						})
 						$("#alt").show()
 					}
@@ -72,3 +72,7 @@
 					}
 				)
 			</script>
+
+
+
+
